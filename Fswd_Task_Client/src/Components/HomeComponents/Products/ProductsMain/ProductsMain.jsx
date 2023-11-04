f5f4c746cb4dfc70384b1f52e6a29f61ded10a5b
unknown <mdtanvir7462@gmail.com>
2023-11-04 12:13:29 +0600

    a spiner added

--- Fswd_Task_Client/src/Components/HomeComponents/Products/ProductsMain/ProductsMain.jsx
+++ Fswd_Task_Client/src/Components/HomeComponents/Products/ProductsMain/ProductsMain.jsx
@@ -10,7 +10,6 @@ const ProductsMain = () => {
         .then((res) => res.json())
         .then((data) => {
           setProducts(data.slice(0, 8));
-        
         });
     } catch (err) {
       console.log(err);
@@ -21,6 +20,15 @@ const ProductsMain = () => {
       <h2 className="  text-2xl md:text-2xl font-semibold text-center  mt-10 md:mt-0 mb-5 md:mb-7 lg:mb-8 text-white ">
         Our Populer Product's
       </h2>
+      {Products?.length === 0 && (
+        <p className=" flex items-center justify-center text-center h-[30dvh] md:h-[50dvh] ">
+          {" "}
+          <button className="btn  text-white">
+            <span className="loading loading-spinner"></span>
+            Wait a second
+          </button>
+        </p>
+      )}
       <div className="grid gap-4 md:gap-5 lg:gap-6 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4  w-[85%] mx-auto">
         {Products?.map((product) => (
           <ProductCard product={product} key={product.id}></ProductCard>
